e65d5f626fb872b340525590502c926a927e3642
audri998 <72659585+audri998@users.noreply.github.com>
2020-12-14 22:48:31 -0500

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class paper {
+
+constructor(x,y,r){
+	var options ={
+		isStatic:false,
+		restitution:0.3,
+		friction:0.5,
+		density:1.2
+}
+this.x=x
+this.y=y
+this.r=r
+this.Body=Bodies.circle(x,y,r/2,options) 
+World.add(world,this.Body)
+
+}
+display(){
+	var pos=this.Body.position 
+	//push();
+	//translate(pos.x,pos.y);
+	ellipse(pos.x,pos.y,this.r,this.r);
+	//pop();
+}
+
+
+}
+
